da8c60ab71b88ce37f194d42676534dce1a06305
dukpe <daniellamodukpe@gmail.com>
2025-01-30 10:50:06 +0100

    small changes

--- dukpe_backend_stage0.py
+++ dukpe_backend_stage0.py
@@ -1,15 +1,17 @@
 from flask import Flask, Response  
+from flask_cors import CORS
 from datetime import datetime
 import json  
 
 app = Flask(__name__)  
+CORS(app)
 
 @app.route('/', methods=['GET'])  
 def get_info():  
     response = {
         "email": "daniellamodukpe@gmail.com",
         "current_datetime": datetime.utcnow().isoformat() + 'Z', 
-        "github_url": "https://github.com/dany-gaga/HNG12_task"  
+        "github_url": "<https://github.com/dany-gaga/HNG12_task>"  
     }  
     json_response = json.dumps(response, sort_keys=False)
     return Response(json_response, mimetype = 'application/json')  
