020016c7e9301ee9d4d9d9fa686ed9d7d66daa6a
dukpe <daniellamodukpe@gmail.com>
2025-01-30 09:50:49 +0100

    made changes to order the instances

--- dukpe_backend_stage0.py
+++ dukpe_backend_stage0.py
@@ -1,17 +1,18 @@
 from flask import Flask, jsonify  
 from flask_cors import CORS  
-from datetime import datetime  
+from datetime import datetime 
+from collections import OrderedDict 
 
 app = Flask(__name__)  
 CORS(app)  # Enable CORS for all routes  
 
 @app.route('/', methods=['GET'])  
 def get_info():  
-    response = {  
-        "email": "daniellamodukpe@gmail.com",  # Replace with your email  
-        "current_datetime": datetime.utcnow().isoformat() + 'Z',  
-        "github_url": "https://github.com/dany-gaga/HNG12_task"  # Update with your GitHub URL  
-    }  
+    response = OrderedDict([  
+        ("email": "daniellamodukpe@gmail.com"),  
+        ("current_datetime": datetime.utcnow().isoformat() + 'Z'),  
+        ()"github_url": "https://github.com/dany-gaga/HNG12_task")  
+                           ]) 
     return jsonify(response)  
 
 if __name__ == '__main__':  
