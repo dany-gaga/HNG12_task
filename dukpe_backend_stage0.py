64439b17edc55f9a649f98556c84b0c29378903c
dukpe <daniellamodukpe@gmail.com>
2025-01-30 10:13:02 +0100

    json used instead of jsonify

--- dukpe_backend_stage0.py
+++ dukpe_backend_stage0.py
@@ -1,19 +1,18 @@
-from flask import Flask, jsonify  
-from flask_cors import CORS  
-from datetime import datetime 
-from collections import OrderedDict 
+from flask import Flask, Response  
+from datetime import datetime
+import json  
 
 app = Flask(__name__)  
-CORS(app)  # Enable CORS for all routes  
 
 @app.route('/', methods=['GET'])  
 def get_info():  
-    response = OrderedDict([  
-        ("email": "daniellamodukpe@gmail.com"),  
-        ("current_datetime": datetime.utcnow().isoformat() + 'Z'),  
-        ("github_url": "https://github.com/dany-gaga/HNG12_task")  
-                           ]) 
-    return jsonify(response)  
+    response = {
+        "email": "daniellamodukpe@gmail.com",
+        "current_datetime": datetime.utcnow().isoformat() + 'Z', 
+        "github_url": "https://github.com/dany-gaga/HNG12_task"  
+    }  
+    json_response = json.dumps(response)
+    return Response(json_response, mimetype = 'application/json')  
 
 if __name__ == '__main__':  
     app.run(debug=True)  
